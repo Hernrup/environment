4cc496d5a9c3f00e549fe1696499ff0d6e9a7bad
Mikael Hernrup <mikael@hernrup.se>
2017-06-28 14:48:06 +0200

    add fzf

--- tasks/unix/shell.sh
+++ tasks/unix/shell.sh
@@ -3,3 +3,6 @@ set -e
 
 sudo apt install zsh
 git clone https://github.com/chriskempson/base16-shell.git /home/mhe/.config/base16-shell || true
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.build/fzf || true
+~/.build/fzf/install
