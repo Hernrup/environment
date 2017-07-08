293a6bfae3c75e1079886c428cfac80c33d0cb83
Mikael Hernrup <mikael@hernrup.se>
2017-07-08 13:32:46 +0200

    add tmux plugin manager

--- tasks/unix/core.sh
+++ tasks/unix/core.sh
@@ -2,3 +2,6 @@
 set -e
 sudo apt update
 sudo apt install git tmux htop curl zip unzip build-essential libssl-dev g++ zsh openssl cmake -y
+
+# Fetch tmux plugin manager
+git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
