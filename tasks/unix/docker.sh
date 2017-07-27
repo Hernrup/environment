d1df1bfd602a59f1d9bee632a3ad6fddffb031ea
Mikael Hernrup <mikael@hernrup.se>
2017-07-27 12:19:02 +0200

    install docker-compose from git

--- tasks/unix/docker.sh
+++ tasks/unix/docker.sh
@@ -2,5 +2,18 @@
 set -e
 
 echo 'install docker'
-sudo apt install docker-ce docker-compose -y
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+# docker
+sudo apt install docker-ce -y
+
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
+sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo usermod -aG docker mhe
