2e9681b8146d9706b605cf0d059ef3a79c6885fb
Mikael Hernrup <mikael@hernrup.se>
2017-10-10 20:39:06 +0200

    fix persmission issue with backup actions

--- nodes/srv/backupninja/run.sh
+++ nodes/srv/backupninja/run.sh
@@ -7,6 +7,12 @@ STDERR_LOC=${STDERR_LOC:-/proc/1/fd/2}
 
 BACKUP_DECLARATIONS=${BACKUP_DECLARATIONS:-/etc/backup.d}
 
-echo "* * * * * chmod -R 700 ${BACKUP_DECLARATIONS}; chown -R root:root ${BACKUP_DECLARATIONS}; chmod -R 600 ${BACKUP_DECLARATIONS}; /usr/sbin/backupninja -d > ${STDOUT_LOC} 2> ${STDERR_LOC}" | crontab -
+cp /etc/backup_actions/* /etc/backup.d/
+
+chmod 700 -R ${BACKUP_DECLARATIONS}
+chown -R root:root ${BACKUP_DECLARATIONS}
+chmod 600 -R ${BACKUP_DECLARATIONS}
+
+echo "* * * * * /usr/sbin/backupninja -d > ${STDOUT_LOC} 2> ${STDERR_LOC}" | crontab -
 
 exec cron -f
