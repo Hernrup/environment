8f46e854150fb8b02c282a30dd8b544ee674cccf
Mikael Hernrup <mikael@hernrup.se>
2017-10-11 15:30:55 +0200

    failed attempt to add mail repoting to backup

--- nodes/srv/backupninja/run.sh
+++ nodes/srv/backupninja/run.sh
@@ -7,12 +7,7 @@ STDERR_LOC=${STDERR_LOC:-/proc/1/fd/2}
 
 BACKUP_DECLARATIONS=${BACKUP_DECLARATIONS:-/etc/backup.d}
 
-cp /etc/backup_actions/* /etc/backup.d/
-
-chmod 700 -R ${BACKUP_DECLARATIONS}
-chown -R root:root ${BACKUP_DECLARATIONS}
-chmod 600 -R ${BACKUP_DECLARATIONS}
+. /prepare.sh
 
 echo "* * * * * /usr/sbin/backupninja -d > ${STDOUT_LOC} 2> ${STDERR_LOC}" | crontab -
-
 exec cron -f
--- nodes/srv/backupninja/run_now.sh
+++ nodes/srv/backupninja/run_now.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-STDOUT_LOC=${STDOUT_LOC:-/proc/1/fd/1}
-STDERR_LOC=${STDERR_LOC:-/proc/1/fd/2}
-
-BACKUP_DECLARATIONS=${BACKUP_DECLARATIONS:-/etc/backup.d}
-
-cp /etc/backup_actions/* /etc/backup.d/
-
-chmod 700 -R ${BACKUP_DECLARATIONS}
-chown -R root:root ${BACKUP_DECLARATIONS}
-chmod 600 -R ${BACKUP_DECLARATIONS}
+. /prepare.sh
 
 exec /usr/sbin/backupninja -d -n
