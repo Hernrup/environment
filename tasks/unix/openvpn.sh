14ff2d6a04c7abff9277e566be2f43126fce6539
Mikael Hernrup <mikael@hernrup.se>
2017-11-13 10:52:18 +0100

    dont use automatic resolver for openvpn

--- tasks/unix/openvpn.sh
+++ tasks/unix/openvpn.sh
@@ -2,8 +2,8 @@
 set -e
 
 sudo apt install openvpn
-git clone https://github.com/masterkorp/openvpn-update-resolv-conf ~/.build/openvpn-update-resolv-conf
-cp ~/.build/openvpn-update-resolv-conf/update-resolv-conf.sh /usr/local/bin/
+# git clone https://github.com/masterkorp/openvpn-update-resolv-conf ~/.build/openvpn-update-resolv-conf
+# cp ~/.build/openvpn-update-resolv-conf/update-resolv-conf.sh /usr/local/bin/
 
 # add the following lines to the ovpn config file manualy if you dont have an existing file
 #script-security 2
