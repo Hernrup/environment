16aa33dbfae1d98c722a30a08979035da5141e09
Mikael Hernrup <mikael@hernrup.se>
2017-11-23 10:05:13 +0100

    add fzf install params

--- tasks/unix/shell.sh
+++ tasks/unix/shell.sh
@@ -5,4 +5,4 @@ sudo apt install zsh
 git clone https://github.com/chriskempson/base16-shell.git /home/mhe/.config/base16-shell || true
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.build/fzf || true
-~/.build/fzf/install
+~/.build/fzf/install --no-key-bindings --64 --completion --no-update-rc
